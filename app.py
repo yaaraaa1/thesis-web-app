6c5a3316d29af07edf05b1cfeb2a0ad55e2cb531
Yaara Aybar <yaara.aybar@yale.edu>
2024-03-18 20:02:01 -0700

    Got the app to work again!

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ the_app = Flask(__name__)
 token = os.environ.get('REPLICATE_API_TOKEN')
 
 UPLOAD_FOLDER = 'uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+the_app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
@@ -23,7 +23,7 @@ if not os.path.exists(UPLOAD_FOLDER):
 @app.route('/')
 def home():
     return render_template('index.html')
-    
+
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
